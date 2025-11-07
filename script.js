f3685142c59aca009a25699fa657cb25ed62b65a
GAURAV  TAYADE <110455312+GauravTayadeDevloper@users.noreply.github.com>
2025-11-07 11:04:59 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,7 @@
 function toFahrenheit(celsius) {
  // Write your code here
-	return (celsius × 9/5) + 32; 
+	return (celsius × 9/5) + 32;
 }
-
 // // Do not change the code below
 const celsius = prompt("Enter Celsius:");
 alert(toFahrenheit(Number(celsius)));
